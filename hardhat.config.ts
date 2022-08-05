adf4a92b9b4a14ecd2a35b08dd0f56ef772e4c27
hcheng826 <lilioo826@gmail.com>
2022-08-05 16:14:11 +0800

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+      accounts: [process.env.PRIVATE_KEY ?? '']
+    },
+    // mainnet: { ... }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API
+  }
 };
 
 export default config;
